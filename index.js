08d49c7833e46601c25f26e61474f8e5e5f216cf
GodBrand69 <97448645+GodBrand69@users.noreply.github.com>
2023-02-22 18:39:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+
+    playSound(buttonInnerHTML);
+
+    animation(buttonInnerHTML);
+  })
+
+  document.addEventListener("keydown", function (event) {
+    playSound(event.key);
+
+    animation(event.key)
+  });
+
+  function playSound(key) {
+
+    switch (key) {
+      case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+
+      case "l":
+        var kickBass = new Audio('sounds/kick-bass.mp3');
+        kickBass.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+
+  }
+  }
+  // );
+
+function animation (currentKey) {
+      document.querySelector("." + currentKey).classList.add("pressed");
+
+      setTimeout(function () {
+        document.querySelector("." + currentKey).classList.remove("pressed");
+      }, 100);
+}
+
+
+// function Audio(fileLocation) {
+//   this.fileLocation = fileLocation;
+//   this.play = function () {
+//     var tom1 = new Audio('sounds/tom1.mp3');
+//     tom1.play();
+//   }
+// }
+
+
+
+
+}
